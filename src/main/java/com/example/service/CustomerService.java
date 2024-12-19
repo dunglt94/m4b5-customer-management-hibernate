6262057470a9cdb87adee637ed80b3cb92e32fe5
dunglt94 <tuandung9594@gmail.com>
2024-12-19 15:13:59 +0700

    first add

---
+++ src/main/java/com/example/service/CustomerService.java
@@ -0,0 +1,91 @@
+package com.example.service;
+
+import com.example.model.Customer;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import java.util.List;
+
+@Service
+public class CustomerService implements ICustomerService {
+    private static SessionFactory sessionFactory;
+    private static EntityManager entityManager;
+
+    static {
+        try {
+            sessionFactory = new Configuration()
+                    .configure("hibernate.conf.xml")
+                    .buildSessionFactory();
+            entityManager = sessionFactory.createEntityManager();
+        } catch (HibernateException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        String queryStr = "SELECT c FROM Customer AS c";
+        TypedQuery<Customer> query = entityManager.createQuery(queryStr, Customer.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public void save(Customer customer) {
+        Transaction transaction = null;
+        Customer origin;
+        if (customer.getId() == 0) {
+            origin = new Customer();
+        } else {
+            origin = findById(customer.getId());
+        }
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            origin.setName(customer.getName());
+            origin.setEmail(customer.getEmail());
+            origin.setAddress(customer.getAddress());
+            session.saveOrUpdate(origin);
+            transaction.commit();
+        } catch (Exception e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+
+    @Override
+    public Customer findById(int id) {
+        String queryStr = "SELECT c FROM Customer AS c WHERE c.id = :id";
+        TypedQuery<Customer> query = entityManager.createQuery(queryStr, Customer.class);
+        query.setParameter("id", id);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public void remove(int id) {
+        Customer customer = findById(id);
+        if (customer != null) {
+            Transaction transaction = null;
+            try (Session session = sessionFactory.openSession()) {
+                transaction = session.beginTransaction();
+                session.delete(customer);
+                transaction.commit();
+            } catch (Exception e) {
+                //noinspection CallToPrintStackTrace
+                e.printStackTrace();
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+            }
+        }
+    }
+}
